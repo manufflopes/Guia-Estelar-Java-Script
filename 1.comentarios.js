11def55ea9c91412c9e1f0487704765660278dde
Manu <manuellafflopes@gmail.com>
2022-07-15 16:21:01 +0100

    Guia Estelar JS

---
+++ 1.comentarios.js
@@ -0,0 +1,8 @@
+console.log('Bem vindos ao Starter')
+
+// o código abaixo escreve alguma mensagem no devtools
+// aqui eu tenho um comentário em linha 
+/* qndo há mais de duas linhas de comentários deve se usar a mesma indicação do html...*/
+
+console.log('Bem vindos ao Starter')
+console.log('não iria ignorar')
