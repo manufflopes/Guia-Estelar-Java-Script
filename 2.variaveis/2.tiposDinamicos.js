cf12ef27ff043d76e2a49d32806b4726c08f7f8b
Manu <manuellafflopes@gmail.com>
2022-07-18 18:16:10 +0100

    Guia Estelar JS

---
+++ 2.variaveis/2.tiposDinamicos.js
@@ -0,0 +1,8 @@
+/* 
+O JS é uma linguagem fracamente tipada e dinâmica
+    * variáveis não precisam ter um tipo previamente definido
+    * podemos mudar o conteúdo da variável
+    * 
+    * let e const são os mais usados
+
+*/
---
+++ 2.variaveis/3.scopeVar.js
@@ -0,0 +1,21 @@
+/* 
+# Scope 
+    * Escopo determina a visibilidade de alguma variavel no JS
+
+# Block statement
+
+    // vamos iniciar um bloco
+    {
+        // aqui dentro é um bloco e posso colocar qualquer código
+    }
+    //aqui fechamos o bloco
+O bloco também criará um novo escopo. Chamamos de 'block-scoped'
+
+# var
+    // var é global e poderá funcionar fora de um escopo de bloco
+    console.log ('> existe x antes do bloo ?', x)
+    {
+        var x = 0
+    }
+    comsole.log('> existe x depois do bloco?', x)
+*/
