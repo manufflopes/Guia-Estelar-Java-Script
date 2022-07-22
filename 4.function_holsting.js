803124fc25a6519751d0c51e6395de461eb3a49d
Manu <manuellafflopes@gmail.com>
2022-07-22 17:54:09 +0100

    Guia Estelar JS

--- 4.function_holsting.js
+++ 4.function_holsting.js
@@ -1,1 +1,15 @@
 // function hoisting
+
+sayMyName()
+
+// function sayMyName() {
+//     console.log('Mike')
+// }
+
+// const sayMyName = function () {
+//     console.log('Mike')
+// }
+
+sayMyName = function () {
+    console.log('Mike')
+}
