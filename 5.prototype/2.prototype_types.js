38fdf8893725dff6e8bb13a45228020876cbce57
Manu <manuellafflopes@gmail.com>
2022-07-25 21:08:19 +0100

    Guia Estelar JS

--- 5.prototype/2.prototype_types.js
+++ 5.prototype/2.prototype_types.js
@@ -127,22 +127,22 @@ console.log(Array.from(senha))
 
 Existem várias maneiras de manipular arrays, como: o método push(), que adiciona um elemento no final do array, o método unshift(), que adiciona um elemento no começo do array, o método pop(), que retira um elemento do final do array, o método shift(), que retira um elemento do começo do array, o método slice(), que recebe como argumentos a posição de início e a posição do fim da parte que você quer selecionar, e retorna os valores destas posições, o método splice(), que recebe como argumentos a posição de início e a quantidade de elementos que você quer remover e o método indexOff(), que encontra o index do valor que recebe como argumento em um array.
 */
-let techs = ["html", "css", "js"]
+let techs = ['html', 'css', 'js']
 
- //* adicionar um item no fim
- techs.push("nodejs")
- //* adicionar no começo
- techs.unshift("sql")
- //* remover do fim
- //techs.pop()
- //* remover do começo
- //techs.shift()
- //* pegar somente alguns elementos do array
- //console.log(techs.slice(1,3))
- //* remover 1 ou mais itens de qualquer posição do array
- //techs.splice(1, 1)
- //* encontrar a posição de um elemnto no array
- let index = techs.indexOf('css')
- techs.splice(index,1)
- 
+//* adicionar um item no fim
+techs.push('nodejs')
+//* adicionar no começo
+techs.unshift('sql')
+//* remover do fim
+//techs.pop()
+//* remover do começo
+//techs.shift()
+//* pegar somente alguns elementos do array
+//console.log(techs.slice(1,3))
+//* remover 1 ou mais itens de qualquer posição do array
+//techs.splice(1, 1)
+//* encontrar a posição de um elemnto no array
+let index = techs.indexOf('css')
+techs.splice(index, 1)
+
 console.log(techs)
