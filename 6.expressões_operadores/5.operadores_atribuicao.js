7d25fbf9820d42087e98253c42910cda0bad59de
Manu <manuellafflopes@gmail.com>
2022-07-27 18:49:10 +0100

    Operadores

---
+++ 6.expressões_operadores/5.operadores_atribuicao.js
@@ -0,0 +1,32 @@
+/*
+    Operadores de atribuição
+
+        Veremos nesta aula sobre operadores de atribuição. Quando falamos em atribuição, queremos na verdade dizer assignment, que já vimos bastante, mas podemos também realizar diferentes tipos de atribuição, usando operadores aritméticos.
+    Exemplos:
+ */
+
+// Operadores de atribuição (Assignment)
+let x
+console.log(x)
+
+// assignment normal:
+x = 1
+console.log(x)
+// addition assignment (adição):
+x += 2
+console.log(x)
+// subtraction assignment (subtração):
+x -= 1
+console.log(x)
+// multiplication assignment (multiplacação):
+x *= 2
+console.log(x)
+// division assignment (divisão):
+x /= 2
+console.log(x)
+// exponetiation assignment (exponenciação):
+x **= 2
+console.log(x)
+// remainder assignment (resto de divisão):
+x %= 2
+console.log(x)
